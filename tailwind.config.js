6e96bd263f136dcd67bb71a71950b9a2fa773b94
sugoii77-rgb <sugoii77@gmail.com>
2025-08-01 09:07:14 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        earth: {
+          ocean: '#1e40af',
+          land: '#065f46'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
