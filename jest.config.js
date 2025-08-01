07594a9d368336cab35efcb629de92b18cef921b
sugoii77-rgb <sugoii77@gmail.com>
2025-08-01 09:06:32 +0900

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapping: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true
+    }]
+  },
+  collectCoverageFrom: [
+    'scripts/**/*.ts',
+    'app/**/*.{ts,tsx}',
+    '!app/main.tsx',
+    '!**/*.d.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
+};
